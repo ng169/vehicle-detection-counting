643ae5cc1db3eff77220fba4ac973abfa315ad9e
ng169 <pokeakshay123@gmail.com>
2023-06-30 02:45:23 +0530

    added comments to speed.py

--- speed.py
+++ speed.py
@@ -1,135 +1,131 @@
 import cv2
 import pandas as pd
-import numpy as np
 from ultralytics import YOLO
-from tracker import*
+from tracker import Tracker
 import time
 from math import dist
-model=YOLO('yolov8s.pt')
 
+# Load YOLO model and initialize the tracker
+model = YOLO('yolov8s.pt')
+tracker = Tracker()
 
+# Set up video capture and create a window for displaying the results
+cv2.namedWindow('Vehicle Detection And Tracking')
+cap = cv2.VideoCapture('veh2.mp4')
 
-cv2.namedWindow('RGB')
-    
-
-cap=cv2.VideoCapture('veh2.mp4')
-
-
+# Read the class labels from a file
 my_file = open("coco.txt", "r")
 data = my_file.read()
-class_list = data.split("\n") 
-#print(class_list)
+class_list = data.split("\n")
 
-count=0
+count = 0
 
-tracker=Tracker()
+# Define the crossing lines and offset for vehicle counting
+crossing_line_y1 = 322
+crossing_line_y2 = 368
+offset = 6
 
-cy1=322
-cy2=368
+# Initialize dictionaries for storing vehicle timestamps and counters
+vehicles_down = {}
+counter_down = []
+vehicles_up = {}
+counter_up = []
+speeds = {}
 
-offset=6
-
-vh_down={}
-counter_up=[]
-
-
-vh_up={}
-counter_down=[]
-
-while True:    
-    ret,frame = cap.read()
+# Main loop for processing each frame of the video
+while True:
+    # Read the frame from the video capture
+    ret, frame = cap.read()
     if not ret:
         break
+
     count += 1
     if count % 3 != 0:
         continue
-    frame=cv2.resize(frame,(1020,500))
-   
 
-    results=model.predict(frame)
-    # print(results)
-    a=results[0].boxes.boxes
-    # print(results[0].boxes)
-    # print(a)
-    px=pd.DataFrame(a).astype("float")
-#    print(px)
-    list=[]
-             
-    for index,row in px.iterrows():
-#        print(row)
- 
-        x1=int(row[0])
-        y1=int(row[1])
-        x2=int(row[2])
-        y2=int(row[3])
-        d=int(row[5])
-        c=class_list[d]
-        if 'car' in c:
-            list.append([x1,y1,x2,y2])
-    bbox_id=tracker.update(list)
-    for bbox in bbox_id:
-        x3,y3,x4,y4,id=bbox
-        cx=int(x3+x4)//2
-        cy=int(y3+y4)//2
-        
-        cv2.rectangle(frame,(x3,y3),(x4,y4),(0,0,255),2)
-        
+    # Resize the frame for faster processing
+    frame = cv2.resize(frame, (1020, 500))
 
+    # Perform object detection using YOLO model
+    results = model.predict(frame)
+    boxes = results[0].boxes.data
 
-        if cy1<(cy+offset) and cy1 > (cy-offset):
-           vh_down[id]=time.time()
-           
-        if id in vh_down:
-          
-           if cy2<(cy+offset) and cy2 > (cy-offset):
-             elapsed_time=time.time() - vh_down[id]
-             if counter_up.count(id)==0:
-                counter_up.append(id)
-                distance = 10 # meters
-                a_speed_ms = distance / elapsed_time
-                a_speed_kh = a_speed_ms * 3.6
-                cv2.circle(frame,(cx,cy),4,(0,0,255),-1)
-                cv2.putText(frame,str(id),(x3,y3),cv2.FONT_HERSHEY_COMPLEX,0.6,(255,255,255),1)
-                cv2.putText(frame,str(int(a_speed_kh))+'Km/h',(x4,y4 ),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),2)
-                
-        #####going UP#####     
-        if cy2<(cy+offset) and cy2 > (cy-offset):
-           vh_up[id]=time.time()
-        if id in vh_up:
+    vehicle_list = []
+    # Extract bounding box coordinates and class labels for vehicles
+    for box in boxes:
+        x1 = int(box[0])
+        y1 = int(box[1])
+        x2 = int(box[2])
+        y2 = int(box[3])
+        class_id = int(box[5])
+        class_name = class_list[class_id]
+        if 'car' in class_name:
+            vehicle_list.append([x1, y1, x2, y2])
 
-           if cy1<(cy+offset) and cy1 > (cy-offset):
-             elapsed1_time=time.time() - vh_up[id]
+    # Update the tracker with the current vehicle list
+    bbox_ids = tracker.update(vehicle_list)
 
- 
+    # Process each tracked vehicle
+    for bbox_id in bbox_ids:
+        x1, y1, x2, y2, vehicle_id = bbox_id
+        center_x = (x1 + x2) // 2
+        center_y = (y1 + y2) // 2
 
+        # Draw a bounding box around the vehicle
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-             if counter_down.count(id)==0:
-                counter_down.append(id)      
-                distance1 = 10 # meters
-                a_speed_ms1 = distance1 / elapsed1_time
-                a_speed_kh1 = a_speed_ms1 * 3.6
-                cv2.circle(frame,(cx,cy),4,(0,0,255),-1)
-                cv2.putText(frame,str(id),(x3,y3),cv2.FONT_HERSHEY_COMPLEX,0.6,(255,255,255),1)
-                cv2.putText(frame,str(int(a_speed_kh1))+'Km/h',(x4,y4),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),2)
+        # Check if vehicle crosses the lower line
+        if crossing_line_y1 - offset < center_y < crossing_line_y1 + offset:
+            vehicles_down[vehicle_id] = time.time()
 
-           
+        # Check if vehicle crosses the upper line after crossing the lower line
+        if vehicle_id in vehicles_down:
+            if crossing_line_y2 - offset < center_y < crossing_line_y2 + offset:
+                elapsed_time = time.time() - vehicles_down[vehicle_id]
+                if vehicle_id not in counter_up:
+                    counter_up.append(vehicle_id)
+                    distance = 10  # meters
+                    speed_ms = distance / elapsed_time
+                    speed_kph = speed_ms * 3.6
+                    speeds[vehicle_id] = speed_kph
 
-    cv2.line(frame,(274,cy1),(814,cy1),(255,255,255),1)
+        # Check if vehicle crosses the upper line
+        if crossing_line_y2 - offset < center_y < crossing_line_y2 + offset:
+            vehicles_up[vehicle_id] = time.time()
 
-    cv2.putText(frame,('L1'),(277,320),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),2)
+        # Check if vehicle crosses the lower line after crossing the upper line
+        if vehicle_id in vehicles_up:
+            if crossing_line_y1 - offset < center_y < crossing_line_y1 + offset:
+                elapsed_time = time.time() - vehicles_up[vehicle_id]
+                if vehicle_id not in counter_down:
+                    counter_down.append(vehicle_id)
+                    distance = 10  # meters
+                    speed_ms = distance / elapsed_time
+                    speed_kph = speed_ms * 3.6
+                    speeds[vehicle_id] = speed_kph
 
+        # Display the speed for each vehicle
+        if vehicle_id in speeds:
+            speed_text = str(int(speeds[vehicle_id])) + ' Km/h'
+            cv2.putText(frame, speed_text, (x2, y2), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 2)
 
-    cv2.line(frame,(177,cy2),(927,cy2),(255,255,255),1)
- 
-    cv2.putText(frame,('L2'),(182,367),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),2)
-    d=(len(counter_up))
-    u=(len(counter_down))
-    cv2.putText(frame,('goingdown:-')+str(d),(60,90),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),2)
+    # Draw crossing lines and display vehicle counts
+    cv2.line(frame, (274, crossing_line_y1), (814, crossing_line_y1), (255, 255, 255), 1)
+    cv2.putText(frame, 'L1', (277, 320), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 2)
 
-    cv2.putText(frame,('goingup:-')+str(u),(60,130),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),2)
-    cv2.imshow("RGB", frame)
-    if cv2.waitKey(1)&0xFF==27:
+    cv2.line(frame, (177, crossing_line_y2), (927, crossing_line_y2), (255, 255, 255), 1)
+    cv2.putText(frame, 'L2', (182, 367), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 2)
+
+    num_vehicles_down = len(counter_up)
+    num_vehicles_up = len(counter_down)
+    cv2.putText(frame, 'goingdown: ' + str(num_vehicles_down), (60, 90), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 2)
+    cv2.putText(frame, 'goingup: ' + str(num_vehicles_up), (60, 130), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 2)
+
+    # Display the frame with vehicle detection and tracking results
+    cv2.imshow("Vehicle Detection And Tracking", frame)
+    if cv2.waitKey(1) & 0xFF == 27:
         break
+
+# Release the video capture and close the display window
 cap.release()
 cv2.destroyAllWindows()
-
